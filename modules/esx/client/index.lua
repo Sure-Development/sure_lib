e0effe9391f089ddb55f7e9666f9a49b337511a1
999s <123440999+mysbryce@users.noreply.github.com>
2025-09-23 18:12:09 +0700

    feat: add GetItem function for client

--- modules/esx/client/index.lua
+++ modules/esx/client/index.lua
@@ -1,5 +1,6 @@
 local esx = {}
 local playerLoaded = promise.new()
+local itemIndexes = {}
 
 CreateThread(function(threadId)
     while not ESX.IsPlayerLoaded() do
@@ -12,6 +13,24 @@ end)
 
 function esx.WaitPlayerLoaded()
     Citizen.Await(playerLoaded)
+end
+
+--- @param name string
+--- @return ESXItem?
+function esx.GetItem(name)
+    local index = itemIndexes[name]
+    local inventory = ESX.GetPlayerData().inventory
+    if index == nil then
+        for k, v in ipairs(inventory) do
+            if v.name == name then
+                itemIndexes[name] = k
+                index = k
+                break
+            end
+        end
+    end
+
+    return inventory[index]
 end
 
 return esx
