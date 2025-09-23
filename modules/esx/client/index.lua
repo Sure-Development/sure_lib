eed08e70448a41ec19fbcf95abc0ceb09b84466a
999s <123440999+mysbryce@users.noreply.github.com>
2025-09-24 06:07:45 +0700

    feat: add helper function to call to server with xPlayer action easily

--- modules/esx/client/index.lua
+++ modules/esx/client/index.lua
@@ -52,4 +52,115 @@ function esx.GetAccount(name)
     return accountIndexes[index]
 end
 
+--- @param payload { [1]: integer, [2]: any[] }[]
+--- @return boolean
+function esx.Transactions(payload)
+    local result = true
+    if #payload > 0 then
+        result = lib.callback.await('esx:transactions', false, payload) --[[@as boolean]]
+    end
+
+    return result
+end
+
+--- @param payload { name: string, count: number }[]
+--- @return boolean
+function esx.AddItems(payload)
+    local action = 1
+    local newPayload = {}
+    for _, v in ipairs(payload) do
+        newPayload[#newPayload + 1] = { v.name, v.count }
+    end
+
+    local result = esx.Transactions({
+        [1] = { action, newPayload }
+    })
+
+    return result
+end
+
+--- @param name string
+--- @param count integer
+--- @return boolean
+function esx.AddItem(name, count)
+    return esx.AddItems({
+        [1] = { name = name, count = count }
+    })
+end
+
+--- @param payload { name: string, count: number }[]
+--- @return boolean
+function esx.RemoveItems(payload)
+    local action = -1
+    local newPayload = {}
+    for _, v in ipairs(payload) do
+        newPayload[#newPayload + 1] = { v.name, v.count }
+    end
+
+    local result = esx.Transactions({
+        [1] = { action, newPayload }
+    })
+
+    return result
+end
+
+--- @param name string
+--- @param count integer
+--- @return boolean
+function esx.RemoveItem(name, count)
+    return esx.RemoveItems({
+        [1] = { name = name, count = count }
+    })
+end
+
+--- @param payload { name: string, count: number }[]
+--- @return boolean
+function esx.AddAccounts(payload)
+    local action = 2
+    local newPayload = {}
+    for _, v in ipairs(payload) do
+        newPayload[#newPayload + 1] = { v.name, v.count }
+    end
+
+    local result = esx.Transactions({
+        [1] = { action, newPayload }
+    })
+
+    return result
+end
+
+--- @param name string
+--- @param count integer
+--- @return boolean
+function esx.AddAccount(name, count)
+    return esx.AddAccounts({
+        [1] = { name = name, count = count }
+    })
+end
+
+--- @param payload { name: string, count: number }[]
+--- @return boolean
+function esx.RemoveAccounts(payload)
+    local action = -2
+    local newPayload = {}
+    for _, v in ipairs(payload) do
+        newPayload[#newPayload + 1] = { v.name, v.count }
+    end
+
+    local result = esx.Transactions({
+        [1] = { action, newPayload }
+    })
+
+    return result
+end
+
+--- @param name string
+--- @param count integer
+--- @return boolean
+function esx.RemoveAccount(name, count)
+    return esx.RemoveAccounts({
+        [1] = { name = name, count = count }
+    })
+end
+
 return esx
