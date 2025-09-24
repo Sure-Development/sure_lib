ea192b0a7f04ec2209f564c051632b747e99e6a2
999s <123440999+mysbryce@users.noreply.github.com>
2025-09-24 16:43:39 +0700

    test: add testing for reactive-system with esx helper function

--- resources/client/esx/test.lua
+++ resources/client/esx/test.lua
@@ -19,3 +19,29 @@ RegisterCommand('removeaccount', function(_, args)
     esx.RemoveAccount(args[1], tonumber(args[2]))
     print(args[1], args[2])
 end, true)
+
+--[[
+    -------------------------------------------
+    Track Testing (Reactive-System) with ESX
+    -------------------------------------------
+]]
+
+local t = GetModule('Track')
+local Track = t.Track
+local Effect = t.Effect
+
+local itemToAdd, setItemToAdd = Track('item', 'painkiller')
+local addAmount, setAddAmount = Track('amount', 0)
+
+Effect(function()
+    esx.AddItem(itemToAdd(), addAmount())
+    print(itemToAdd(), addAmount())
+end, { addAmount, itemToAdd })
+
+RegisterCommand('setitem', function(_, args)
+    setItemToAdd(args[1])
+end, true)
+
+RegisterCommand('setamount', function(_, args)
+    setAddAmount(tonumber(args[1]))
+end, true)
